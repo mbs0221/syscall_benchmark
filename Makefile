fba91700e87ecff45bc9de312b43dbde8a8aec03
Benshan Mei <mbs2021@163.com>
2024-03-11 14:37:14 +0800

    Refactor benchmarks with Copilot. Add autoconf and automake support.

--- Makefile
+++
@@ -1,4 +0,0 @@
-all:
-	gcc -o syscall_benchmark syscall_benchmark.c -lkeyutils -lnuma
-clean:
-	rm syscall_benchmark
--- syscall_benchmark.c
+++ syscall_benchmark.c
@@ -5,9 +5,13 @@
 #include <sys/types.h>
 #include <time.h>		// time(), clock_gettime()
 #include <sys/ioctl.h>
+#ifdef HAVE_KEYUTILS_H
 #include <keyutils.h>	// add_key
+#endif
 #include <stdlib.h>
+#ifdef HAVE_NUMAIF_H
 #include <numaif.h>		// get_mempolicy
+#endif
 #include <sys/mman.h>	// pkey_alloc, pkeyfree
 #include <unistd.h>
 
@@ -15,197 +19,114 @@
 
 #define ROUNDS 1000000
 
-int main(void)
-{
-	uint32_t t1 = 0;
-	uint32_t t2 = 0;
-	uint32_t tt = 0;
+typedef struct {
+	const char* name;
+	void (*benchmark)(uint32_t*);
+} Benchmark;
+
+void runBenchmark(const Benchmark* benchmark, uint32_t* measurement) {
+	uint32_t t1, t2, tt;
 	uint64_t total = 0;
-	uint32_t measurement[ROUNDS] = {0};
-	
-	// Warmup loop
-	for(int i = 0; i < 55555555; i++);
-	
-	// nothing
-	for (int r = 0; r < ROUNDS; r++)
-	{
+
+	for (int r = 0; r < ROUNDS; r++) {
 		t1 = rdtscp();
-			// Nothing here
+		benchmark->benchmark(measurement);
 		t2 = rdtscp();
-		tt = t2-t1;
+		tt = t2 - t1;
 		measurement[r] = tt;
-		total = total + tt;
+		total += tt;
 	}
-	FILE *output;
-	output = fopen("nothing.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
+
+	FILE* output = fopen(benchmark->name, "w");
+	for (int r = 0; r < ROUNDS; r++) {
 		fprintf(output, "%u\n", measurement[r]);
-	printf("nothing\t\t%lu\n", total / ROUNDS);
-	fclose(output);
-	
-	// getpid
-	total = 0;
-	for (int r = 0; r < ROUNDS; r++)
-	{
-		t1 = rdtscp();
-			getpid();
-		t2 = rdtscp();
-		tt = t2-t1;
-		measurement[r] = tt;
-		total = total + tt;
 	}
-	output = fopen("getpid.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
-		fprintf(output, "%u\n", measurement[r]);
-	printf("getpid\t\t%lu\n", total / ROUNDS);
+	printf("%s\t\t%lu\n", benchmark->name, total / ROUNDS);
 	fclose(output);
-	
-	// getuid
-	total = 0;
-	for (int r = 0; r < ROUNDS; r++)
-	{
-		t1 = rdtscp();
-			getuid();
-		t2 = rdtscp();
-		tt = t2-t1;
-		measurement[r] = tt;
-		total = total + tt;
-	}
-	output = fopen("getuid.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
-		fprintf(output, "%u\n", measurement[r]);
-	printf("getuid\t\t%lu\n", total / ROUNDS);
-	fclose(output);
-	
-	// time
-	total = 0;	
-	for (int r = 0; r < ROUNDS; r++)
-	{
-		t1 = rdtscp();
-			time(NULL);
-		t2 = rdtscp();
-		tt = t2-t1;
-		measurement[r] = tt;
-		total = total + tt;
-	}
-	output = fopen("time.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
-		fprintf(output, "%u\n", measurement[r]);
-	printf("time\t\t%lu\n", total / ROUNDS);
-	fclose(output);
-	
-	// read
-	total = 0;
+}
+
+void benchmarkNothing(uint32_t* measurement) {
+	// Nothing here
+}
+
+void benchmarkGetPid(uint32_t* measurement) {
+	getpid();
+}
+
+void benchmarkGetUid(uint32_t* measurement) {
+	getuid();
+}
+
+void benchmarkTime(uint32_t* measurement) {
+	time(NULL);
+}
+
+void benchmarkRead(uint32_t* measurement) {
 	int fd;
-	for (int r = 0; r < ROUNDS; r++)
-	{
-		t1 = rdtscp();
-			read(fd, 0, 0);
-		t2 = rdtscp();
-		tt = t2-t1;
-		measurement[r] = tt;
-		total = total + tt;
-	}
-	output = fopen("read.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
-		fprintf(output, "%u\n", measurement[r]);
-	printf("read\t\t%lu\n", total / ROUNDS);
-	fclose(output);
-	
-	// ioctl
-	total = 0;
-	for (int r = 0; r < ROUNDS; r++)
-	{
-		t1 = rdtscp();
-			ioctl(fd, 0, 0);
-		t2 = rdtscp();
-		tt = t2-t1;
-		measurement[r] = tt;
-		total = total + tt;
-	}
-	output = fopen("ioctl.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
-		fprintf(output, "%u\n", measurement[r]);
-	printf("ioctl\t\t%lu\n", total / ROUNDS);
-	fclose(output);
-	
-	// add_key - add a key to the kernel's key management facility
-	// $ sudo apt-get install libkeyutils-dev
-	// $ gcc -o syscall_benchmark syscall_benchmark.c -lkeyutils
-	total = 0;
-	for (int r = 0; r < ROUNDS; r++)
-	{
-		t1 = rdtscp();
-			add_key(0, 0, 0, 0, 0);
-		t2 = rdtscp();
-		tt = t2-t1;
-		measurement[r] = tt;
-		total = total + tt;
-	}
-	output = fopen("add_key.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
-		fprintf(output, "%u\n", measurement[r]);
-	printf("add_key\t\t%lu\n", total / ROUNDS);
-	fclose(output);
-	
-	// get_mempolicy - retrieve NUMA memory policy for a thread
-	// $ gcc -o syscall_benchmark syscall_benchmark.c -lnuma
-	total = 0;
-	for (int r = 0; r < ROUNDS; r++)
-	{
-		t1 = rdtscp();
-			get_mempolicy(0, 0, 0, 0, 0);
-		t2 = rdtscp();
-		tt = t2-t1;
-		measurement[r] = tt;
-		total = total + tt;
-	}
-	output = fopen("get_mempolicy.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
-		fprintf(output, "%u\n", measurement[r]);
-	printf("get_mempolicy\t%lu\n", total / ROUNDS);
-	fclose(output);
-	
-	// clock_gettime
+	read(fd, 0, 0);
+}
+
+void benchmarkIoctl(uint32_t* measurement) {
+	int fd;
+	ioctl(fd, 0, 0);
+}
+
+#ifdef HAVE_KEYUTILS_H
+void benchmarkAddKey(uint32_t* measurement) {
+	add_key(0, 0, 0, 0, 0);
+}
+#endif
+
+#ifdef HAVE_NUMAIF_H
+void benchmarkGetMempolicy(uint32_t* measurement) {
+	get_mempolicy(0, 0, 0, 0, 0);
+}
+#endif
+
+void benchmarkClockGettime(uint32_t* measurement) {
 	struct timespec start;
-	total = 0;
-	for (int r = 0; r < ROUNDS; r++)
-	{
-		t1 = rdtscp();
-			clock_gettime(CLOCK_MONOTONIC, &start);
-		t2 = rdtscp();
-		tt = t2-t1;
-		measurement[r] = tt;
-		total = total + tt;
-	}
-	output = fopen("clock_gettime.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
-		fprintf(output, "%u\n", measurement[r]);
-	printf("clock_gettime\t%lu\n", total / ROUNDS);
-	fclose(output);
-	
-	// mincore
+	clock_gettime(CLOCK_MONOTONIC, &start);
+}
+
+void benchmarkMincore(uint32_t* measurement) {
 	#define PAGE_COUNT 1
 	#define PAGE_SIZE 4096
-	uint8_t * buffer;
+	uint8_t* buffer;
 	unsigned char vec[PAGE_COUNT];
-	buffer = (uint8_t *)mmap(NULL, PAGE_COUNT * PAGE_SIZE, PROT_READ | PROT_WRITE,  MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	buffer = (uint8_t*)mmap(NULL, PAGE_COUNT * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 	size_t length = sysconf(_SC_PAGESIZE);
-	// assert(res == 0);
-	for (int r = 0; r < ROUNDS; r++)
-	{
-		t1 = rdtscp();
-			mincore(buffer, length, vec);
-		t2 = rdtscp();
-		tt = t2-t1;
-		measurement[r] = tt;
-		total = total + tt;
+
+	mincore(buffer, length, vec);
+}
+
+int main(void) {
+	uint32_t measurement[ROUNDS] = {0};
+
+	// Warmup loop
+	for (int i = 0; i < 55555555; i++);
+
+	Benchmark benchmarks[] = {
+		{"nothing.txt", benchmarkNothing},
+		{"getpid.txt", benchmarkGetPid},
+		{"getuid.txt", benchmarkGetUid},
+		{"time.txt", benchmarkTime},
+		{"read.txt", benchmarkRead},
+		{"ioctl.txt", benchmarkIoctl},
+#ifdef HAVE_KEYUTILS_H
+		{"add_key.txt", benchmarkAddKey},
+#endif
+#ifdef HAVE_NUMAIF_H
+		{"get_mempolicy.txt", benchmarkGetMempolicy},
+#endif
+		{"clock_gettime.txt", benchmarkClockGettime},
+		{"mincore.txt", benchmarkMincore}
+	};
+
+	int numBenchmarks = sizeof(benchmarks) / sizeof(benchmarks[0]);
+
+	for (int i = 0; i < numBenchmarks; i++) {
+		runBenchmark(&benchmarks[i], measurement);
 	}
-	output = fopen("mincore.txt", "w");
-	for(int r = 0; r < ROUNDS; r++)
-		fprintf(output, "%u\n", measurement[r]);
-	printf("mincore\t\t%lu\n", total / ROUNDS);
-	fclose(output);
-	
+
 	return 0;
 }
